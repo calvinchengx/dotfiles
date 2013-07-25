67cec241c6fec91bc92c4d7a2b90139459519630
Calvin Cheng <linchuan.cheng@gmail.com>
2013-07-25 13:07:33 +0800

    .jshintrc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 ln -s ~/work/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/work/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/work/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/work/dotfiles/.jshintrc ~/.jshintrc
