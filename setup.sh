eab84c15be90bb01a3dcb98ba614f3854e2edcbb
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-20 07:51:41 +0200

    Improve setup.sh to handle Linux installation cleanly

--- setup.sh
+++ setup.sh
@@ -1,19 +1,54 @@
-sudo port install vim +huge +python27 +gtk2 +lua
+#!/usr/bin/env bash
+
+# Determine OS
+if [[ "`uname`" == "Darwin" ]]; then
+    DISTRO="Darwin";
+elif [ "`uname`" == "Linux" ]; then
+    DISTRO="Linux"
+    [[ "`cat /proc/version`" == *"NixOS"* ]] && DISTRO="NixOS"
+else
+    DISTRO=""
+    echo "This operating system is not supported."
+    return 1
+fi
+
+# vim
+if [[ $DISTRO == "Darwin" ]]; then
+    sudo port install vim +huge +python27 +gtk2 +lua
+fi
 ln -s `pwd`/.vimrc $HOME/.vimrc
 ln -s `pwd`/.vimrc_basic $HOME/.vimrc_basic
+vim -c VundleUpdate -c quitall
+
+# tmux
+ln -s `pwd`/.tmux.conf $HOME/.tmux.conf
+
+# .bash_profile
 ln -s `pwd`/.bash_profile $HOME/.bash_profile
+
+# .zprofile
 ln -s `pwd`/.zprofile $HOME/.zprofile
+
+# git
 ln -s `pwd`/.gitconfig $HOME/.gitconfig
 ln -s `pwd`/.gitignore_global $HOME/.gitignore_global
-ln -s `pwd`/.jshintrc $HOME/.jshintrc
-ln -s `pwd`/.eslintrc $HOME/.eslintrc
-ln -s `pwd`/.nixpkgs $HOME/.nixpkgs
-git clone git://github.com/kennethreitz/autoenv.git $HOME/.autoenv
+
+# nvm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
 npm install -g eslint babel-eslint eslint-plugin-react
 
+# javascript
+ln -s `pwd`/.jshintrc $HOME/.jshintrc
+ln -s `pwd`/.eslintrc $HOME/.eslintrc
+
+# nix
+ln -s `pwd`/.nixpkgs $HOME/.nixpkgs
+
+# autoenv
+git clone git://github.com/kennethreitz/autoenv.git $HOME/.autoenv
+
 # YouCompleteMe
-./$HOME/.vim/bundle/YouCompleteMe/install.sh --clang-completer
+.$HOME/.vim/bundle/YouCompleteMe/install.sh --clang-completer
 ln -s `pwd`/.ycm_extra_conf.py $HOME/.vim/.ycm_extra_conf.py
 
 # Valgrind
