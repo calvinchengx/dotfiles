f557a46b87eed9be9fcc73010c9a5bc2c1087e44
Calvin Cheng <linchuan.cheng@gmail.com>
2015-06-02 11:08:20 +0000

    Update .gitignore_global and empty .nixpkgs/config.nix

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,16 +1,2 @@
-pkgs: {
-  vim = {
-    lua = true;
-  };
-  packageOverrides = pkgs: with pkgs; rec {
-
-    vim-env = pkgs.buildEnv {
-      name = "vim-env";
-      paths = with vimPlugins; [
-        YouCompleteMe
-      ];
-    };
-
-  };
-
+{
 }
