332c93c2acea0caf31d033ff00c70f90888c4d33
Calvin Cheng <linchuan.cheng@gmail.com>
2016-05-31 23:43:19 +0800

    homebrew and homebrew cask update and upgrade script

---
+++ homebrew-update-upgrade.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+(set -x; brew update;)
+(set -x; brew cask update;)
+
+(set -x; brew cleanup;)
+(set -x; brew cask cleanup;)
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+casks=( $(brew cask list) )
+
+for cask in ${casks[@]}
+do
+    installed="$(brew cask info $cask | grep 'Not installed')"
+
+    if [[ $installed = *[!\ ]* ]]; then
+        echo "${red}${cask}${reset} requires ${red}update${reset}."
+        (set -x; brew cask uninstall $cask --force;)
+        (set -x; brew cask install $cask --force;)
+    else
+        echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+done
