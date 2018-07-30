ea71b592d4030071d0c7866a3300dbf7a52df631
Calvin Cheng <linchuan.cheng@gmail.com>
2018-07-30 09:32:22 +0800

    Clean up zsh install

--- setup.sh
+++ setup.sh
@@ -76,11 +76,10 @@ if [[ $DISTRO == "Darwin" ]]; then
     brew install zsh
 fi
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm $HOME/.zshrc
 symlink ".zshrc"
 chsh -s `which zsh`
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
